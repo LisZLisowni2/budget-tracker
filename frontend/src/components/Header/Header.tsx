8b27d2e3f14ae7a5f52c05a89f7fff409c53195c
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-19 17:47:35 +0100

    Update Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="h-32 p-10 bg-amber-300">
+        <div className="h-32 p-5">
             <h1 className="sm:text-5xl">Hello</h1>
         </div>
     )
