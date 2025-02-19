9750b4987a50e1aa5aeb045686ba07d0e4764f79
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-19 17:24:24 +0100

    Configure tailwindcss and add Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import Header from './components/Header/Header'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello!</h1>
+      <Header />
     </>
   )
 }
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="h-16 p-6 bg-amber-700">
+            <h1 className="text-5xl">Hello</h1>
+        </div>
+    )
+}
