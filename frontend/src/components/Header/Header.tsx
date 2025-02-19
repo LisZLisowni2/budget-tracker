f424f14be9a5a6ee4888b51c6374b3ed2ff20ed2
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-19 17:31:51 +0100

    Second configuration of tailwindcss

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <div className="h-16 p-6 bg-amber-700">
-            <h1 className="text-5xl">Hello</h1>
+        <div className="h-32 p-10 bg-amber-300">
+            <h1 className="sm:text-5xl">Hello</h1>
         </div>
     )
 }
