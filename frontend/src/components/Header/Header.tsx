30b50926efcbcaa44cdee47847ad8d07725dc898
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-21 18:27:19 +0100

    Update style of Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 export default function Header() {
     return (
-        <div className="h-32 p-5">
-            <h1 className="sm:text-5xl">Hello</h1>
+        <div className="h-auto text-white p-5 flex justify-between bg-violet-800">
+            <h1 className="text-2xl md:text-3xl lg:text-5xl">BudgetTracker</h1>
+            <div className="flex justify-center *:mx-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 lg:size-12">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            </div>
         </div>
     )
 }
