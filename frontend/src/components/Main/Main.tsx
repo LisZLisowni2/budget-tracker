87b7d9ee3bb5bc35e7a782ccb62e7c098c1b953f
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-28 17:45:16 +0100

    Add Button component to Main and toggle dark

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
+import Button from "../Button/Button"
+
 export default function Main() {
     return (
         <div className="relative bg-rose-300 p-16 lg:p-32 rounded-4xl text-center max-w-3xs lg:max-w-3xl z-10 shadow-2xl backdrop-blur-3xl">
             <h2 className="text-xl lg:text-4xl mb-4">Take control over your budget with BudgetTracker!</h2>
             <p className="text-sm lg:text-1xl">Easy to use, functional UI, no ads, account system for multiple spheres (home, business)</p>
-            <p><button className="mt-5 border p-4 rounded-4xl text-xl font-semibold hover:bg-gray-200 hover:text-black transition hover:shadow-2xl cursor-pointer">Get started</button></p>
+            <p><Button text="Get Started" /></p>
         </div>
     )
 }
