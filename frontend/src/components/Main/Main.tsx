24c1363e7b7ddb0738d250d5c93838f7ac4b729d
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-28 17:37:24 +0100

    Update style of web app and add Button component

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,8 +1,9 @@
 export default function Main() {
     return (
-        <div className="relative bg-rose-300 p-16 lg:p-32 rounded-4xl text-center max-w-3xs lg:max-w-3xl z-10 before:absolute before:bg-indigo-300 before:blur-sm before:p-16 before:lg:p-32 before:rounded-4xl before:-inset-3 before:-z-10">
+        <div className="relative bg-rose-300 p-16 lg:p-32 rounded-4xl text-center max-w-3xs lg:max-w-3xl z-10 shadow-2xl backdrop-blur-3xl">
             <h2 className="text-xl lg:text-4xl mb-4">Take control over your budget with BudgetTracker!</h2>
             <p className="text-sm lg:text-1xl">Easy to use, functional UI, no ads, account system for multiple spheres (home, business)</p>
+            <p><button className="mt-5 border p-4 rounded-4xl text-xl font-semibold hover:bg-gray-200 hover:text-black transition hover:shadow-2xl cursor-pointer">Get started</button></p>
         </div>
     )
 }
