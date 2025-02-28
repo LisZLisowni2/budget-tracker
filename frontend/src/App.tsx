3b0db15507bcd16ea69846c7827de30311d26899
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-02-28 17:13:25 +0100

    Add Routing and update style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import Header from './components/Header/Header'
 import Content from './components/Content/Content'
 import Main from './components/Main/Main'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
@@ -9,12 +10,16 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <section className="h-screen flex text-white flex-col">
-      <Header />
-      <Content>
-        <Main />
-      </Content>
-    </section>
+    <Router>
+      <section className="h-screen flex text-white flex-col">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Content>
+            <Main />
+          </Content>} />
+          </Routes>
+      </section>
+    </Router>
   )
 }
 
--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,6 +1,6 @@
 export default function Main() {
     return (
-        <div className="relative bg-violet-950 p-16 lg:p-32 rounded-4xl text-center max-w-3xs lg:max-w-3xl z-10 before:absolute before:bg-violet-950 before:blur-sm before:p-16 before:lg:p-32 before:rounded-4xl before:-inset-3 before:-z-10">
+        <div className="relative bg-rose-300 p-16 lg:p-32 rounded-4xl text-center max-w-3xs lg:max-w-3xl z-10 before:absolute before:bg-indigo-300 before:blur-sm before:p-16 before:lg:p-32 before:rounded-4xl before:-inset-3 before:-z-10">
             <h2 className="text-xl lg:text-4xl mb-4">Take control over your budget with BudgetTracker!</h2>
             <p className="text-sm lg:text-1xl">Easy to use, functional UI, no ads, account system for multiple spheres (home, business)</p>
         </div>
