0fea75a9ff6852b84481de8589e8c9a3f3aec6e3
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-03-01 12:01:05 +0100

    Install dotenv and add config

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
 const ADDRESS = '0.0.0.0';
+const config = {
+    MongoDB_URI: process.env.MongoURI,
+    RedisDB_URI: process.env.RedisURI
+}
 app.use(express.json());
 
 app.listen(PORT, ADDRESS, () => {
