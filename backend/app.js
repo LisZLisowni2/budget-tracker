a201eb4644d16c53d3d9405d3d676986ee287a55
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-03-02 14:22:57 +0100

    Update app.js and docker-compose.yml

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,45 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+const mongoose = require('mongoose')
+const redis = require('redis')
 
 const app = express();
 const PORT = process.env.Port || 3000;
 const ADDRESS = proccess.env.Address || '0.0.0.0';
 const config = {
-    MongoDB_URI: process.env.MongoURI,
-    RedisDB_URI: process.env.RedisURI,
+    MongoDB_URI: 'mongodb://database:27017/',
+    RedisDB_URI: 'redis://redis:6379',
     JWT_Secret: proccess.env.JWTSecret,
     Port: PORT,
     Address: ADDRESS,
 }
+
+let redisClient
+
+(async () => {
+    redisClient = redis.createClient({
+        url: config.RedisDB_URI
+    })
+
+    redisClient.on('error', (error) => {
+        console.error(`Error: ${error}`)
+    })
+
+    await redisClient.connect();
+
+    console.log("Connected to Redis DB");
+})
+
+mongoose.connect(config.MongoDB_URI);
+
+const mongoClient = mongoose.connection
+mongoClient.on('error', () => {
+    console.error.bind(console, ("Connection error: "))
+})
+mongoClient.once('open', () => {
+    console.log("Connected to MongoDB");
+})
 
 const userRouter = require('./routers/user')(config)
 
