f77dc96bda3911d719c3b71210a220b49c00e577
lislisowni2 <rafalkozikowski735@gmail.com>
2025-09-09 19:12:39 +0200

    Add first tests

---
+++ frontend/src/components/Content/Content.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react"
+import { vi } from "vitest" 
+import { MemoryRouter } from "react-router"
+import "@testing-library/jest-dom"
+import Content from "./Content"
+import HeroBox from "../Main/HeroBox"
+
+describe("Content component", () => {
+    it("renders with single text", () => {
+        render(<Content children={ "Hello!" } />)
+        expect(screen.getByText("Hello!")).toBeInTheDocument()
+        expect(screen.getByText("Hello!")).toHaveTextContent("Hello!")
+    })
+
+    it("renders with complicated component", () => {
+        render(
+            <MemoryRouter>
+                <Content>
+                    <HeroBox />
+                </Content>
+            </MemoryRouter>)
+        expect(screen.getByText("Take control over your budget with BudgetTracker!")).toBeInTheDocument()
+        expect(screen.getByText("Easy to use, functional UI, no ads, account system for multiple spheres (home, business)")).toBeInTheDocument()
+        expect(screen.getByRole('button', { 'name': /Get Started/i })).toBeInTheDocument()
+    })
+})
