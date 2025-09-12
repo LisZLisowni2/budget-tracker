71d85ee65cb3acae4ceede63059b71c023ac87ae
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-09-12 10:17:38 +0200

    Update Notes.tsx

--- frontend/src/components/Dashboard/Notes.tsx
+++ frontend/src/components/Dashboard/Notes.tsx
@@ -1,17 +1,33 @@
 import { useUser } from "../../context/UserContext"
 import Button from "../Button/Button"
 
-function NoteElement() {
+interface INoteElement {
+    id: number,
+    title: string,
+    content: string,
+    ownedBy?: string
+}
+
+function NoteElement({ id, title, content, ownedBy }: INoteElement) {
     return (
-        <div className="flex flex-col justify-start text-left shadow-lg rounded-4xl p-4 m-4 mx-12 hover:scale-105 transition-all">
-            <h1 className="text-3xl break-words max-h-32 overflow-auto">Title</h1>
-            <h3 className="text-sm text-gray-400 h-4 overflow-hidden">Characters</h3>
-        </div>
+        <button id={`note${id}`} className="shadow-lg w-4/5 rounded-4xl mx-12 hover:scale-105 transition-all m-4">
+            <div className="flex flex-col justify-start text-left p-2 m-4">
+                <h1 className="text-3xl break-words max-h-32 overflow-auto">{ title }</h1>
+                <h3 className="text-sm text-gray-400 h-4 overflow-hidden">{ content }</h3>
+            </div>
+        </button>
     )
 }
 
 export default function Notes() {
     const { user, loading } = useUser()
+
+    const listTemplate = {
+        id: 10294,
+        ownedBy: 'Janusz',
+        title: 'Title',
+        content: '<h1>Hello world</h1>'
+    }
 
     if (loading) {
         return (<p>
@@ -24,17 +40,11 @@ export default function Notes() {
     return (
         <div className="flex flex-1 overflow-hidden items-center justify-between flex-row max-lg:overflow-auto">
             <div className="max-md:w-full w-2/5 h-screen overflow-y-auto transition-all shadow-2xl border-r-2">
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
-                <NoteElement />
+                <div className="h-1/16 sticky top-0">
+                    { /* ICONS */ }
+                </div>
+                <hr className="border "/>
+                <NoteElement id={listTemplate.id} title={listTemplate.title} content={listTemplate.content}/>
             </div>
             <div className="max-md:w-1/4 w-4/5 h-full">
             </div>
