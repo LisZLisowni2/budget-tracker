28133bfad60f0bc6dfc9107593f61f39fc608a79
lislisowni2 <rafalkozikowski735@gmail.com>
2025-09-13 19:40:40 +0200

    Update Notes.tsx

--- frontend/src/components/Dashboard/Notes.tsx
+++ frontend/src/components/Dashboard/Notes.tsx
@@ -1,18 +1,21 @@
 import { useUser } from "../../context/UserContext"
+import { useState, MouseEventHandler } from "react"
 
 interface INoteElement {
-    id: number,
+    id?: number,
     title: string,
     content: string,
+    onClick: MouseEventHandler<HTMLButtonElement>,
+    selected: boolean,
     ownedBy?: string
 }
 
-function NoteElement({ id, title, content }: INoteElement) {
+function NoteElement({ title, content, onClick, selected }: INoteElement) {
     return (
-        <button id={`note${id}`} className="shadow-lg w-4/5 rounded-4xl mx-12 hover:scale-105 transition-all m-4">
+        <button id={`note`} onClick={onClick} className={`${(selected) ? "bg-rose-100" : ""} shadow-lg w-4/5 rounded-4xl mx-12 hover:scale-105 transition-all m-4 cursor-pointer`}>
             <div className="flex flex-col justify-start text-left p-2 m-4">
                 <h1 className="text-3xl break-words max-h-32 overflow-auto">{ title }</h1>
-                <h3 className="text-sm text-gray-400 h-4 overflow-hidden">{ content }</h3>
+                <h3 className="text-sm text-gray-400 h-8 overflow-hidden">{ content }</h3>
             </div>
         </button>
     )
@@ -20,6 +23,7 @@ function NoteElement({ id, title, content }: INoteElement) {
 
 export default function Notes() {
     const { user, loading } = useUser()
+    const [ selectedID, setSelectedID ] = useState<number>()
 
     const listTemplate = {
         id: 10294,
@@ -27,6 +31,24 @@ export default function Notes() {
         title: 'Title',
         content: '<h1>Hello world</h1>'
     }
+
+    const listTemplate2 = {
+        id: 1581,
+        ownedBy: 'Janusz',
+        title: 'Koza',
+        content: 'Ale beka krwa'
+    }
+
+    const listTemplate3 = {
+        id: 52,
+        ownedBy: 'Kowal',
+        title: 'Szansa',
+        content: 'Jutro kiedykolwiek będzie mocno w kij'
+    }
+
+    const list = [listTemplate, listTemplate2, listTemplate3]
+
+    let fillteredList = list.filter((note) => note.ownedBy === 'Janusz')
 
     if (loading) {
         return (<p>
@@ -43,9 +65,15 @@ export default function Notes() {
                     { /* ICONS */ }
                 </div>
                 <hr className="border "/>
-                <NoteElement id={listTemplate.id} title={listTemplate.title} content={listTemplate.content}/>
+                {
+                    fillteredList.map((note) => { 
+                        if (note.id === selectedID) return <NoteElement key={note.id} selected={true} title={note.title} content={note.content} onClick={() => setSelectedID(note.id)}/>
+                        return <NoteElement key={note.id} selected={false} title={note.title} content={note.content} onClick={() => setSelectedID(note.id)}/>
+                    })
+                }
             </div>
             <div className="max-md:w-1/4 w-4/5 h-full">
+                {/* SERVER IMPLEMENTATION */}
             </div>
         </div>
     )
