4a3faa654193a36b0a5d73766d5a4dc9cd80bf32
lislisowni2 <rafalkozikowski735@gmail.com>
2025-09-14 18:03:53 +0200

    Update Goal.tsx

--- frontend/src/components/Dashboard/Goals.tsx
+++ frontend/src/components/Dashboard/Goals.tsx
@@ -17,32 +17,17 @@ export default function Goals() {
             <div className="bg-white w-4/5 shadow-2xl rounded-4xl max-h-150 m-8 overflow-auto">
                 <table className="**:p-3 **:border border-collapse w-full h-full table-auto">
                     <thead className="bg-gray-300 sticky top-0">
-                        <tr><th>Date</th><th>Transaction Name</th><th>Receiver?</th><th>Quantity</th></tr>
+                        <tr><th>Date Creation</th><th>Goal</th><th>Required money</th><th>Done?</th><th>Date Complete</th><th></th></tr>
                     </thead>
                     <tbody className="*:hover:bg-gray-100 *:transition-all">
-                        <tr><td>2025-08-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
-                        <tr><td>2025-09-01</td><td>Transaction Name</td><td>True</td><td>99.99</td></tr>
+                        <tr><td>2025-08-01</td><td>Birman Cat</td><td>8000 zł</td><td>true</td><td>2025-09-01</td><td className="*:m-auto *:text-white"><Button text="Complete" onClick={() => console.log()}/></td></tr>
                     </tbody>
                 </table>
             </div>
             <div className="text-white flex justify-evenly w-full h-1/3 items-center">
-                <Button text="Add new transaction"/>
-                <Button text="Change transaction details"/>
-                <Button text="Delete transactions"/>
+                <Button text="Add new goal"/>
+                <Button text="Change goal details"/>
+                <Button text="Delete goal"/>
             </div>
         </div>
     )
