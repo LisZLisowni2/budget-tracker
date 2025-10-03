37174c233589492560a4e0aba9b16bc7abe253ed
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-10-03 10:21:19 +0200

    Update notes.tsx

--- frontend/src/components/Dashboard/Notes.tsx
+++ frontend/src/components/Dashboard/Notes.tsx
@@ -22,7 +22,7 @@ function NoteElement({ title, content, onClick, selected }: INoteElement) {
     return (
         <button id={`note`} onClick={onClick} className={`${(selected) ? "bg-rose-100" : ""} shadow-lg w-4/5 rounded-4xl mx-12 hover:scale-105 transition-all m-4 cursor-pointer`}>
             <div className="flex flex-col justify-start text-left p-2 m-4">
-                <h1 className="text-3xl break-words max-h-32 overflow-auto">{ title }</h1>
+                <h1 className="text-3xl break-words max-h-10 overflow-hidden">{ title }</h1>
                 <h3 className="text-sm text-gray-400 h-8 overflow-hidden">{ content }</h3>
             </div>
         </button>
@@ -58,7 +58,9 @@ export default function Notes() {
     const [ selectedNoteID, setSelectedNoteID ] = useState<number | null>()
 
     const selectedNote = notes.find((note) => note.id === selectedNoteID)
-    let fillteredList = notes.filter((note) => note.ownedBy === 'Janusz')
+    const fillteredList = notes.filter((note) => note.ownedBy === 'Janusz')
+    console.log(selectedNoteID)
+    console.log(selectedNote)
 
     if (loading) {
         return (<p>
@@ -90,19 +92,39 @@ export default function Notes() {
         }
     }
 
+    const createNewNote = () => {
+        const generatedID = Math.random() % 100 + 1
+        setNotes([...notes, {id: generatedID, title: "", content: "", ownedBy: "Janusz"}])
+        setSelectedNoteID(generatedID)
+    }
+
+    const deleteNote = () => {
+        if (selectedNoteID) {
+            setNotes(prevNotes => prevNotes.filter(note => note.id !== selectedNoteID))
+            setSelectedNoteID(null)
+        }
+    }
+
+    const copyNote = () => {
+        if (selectedNoteID && selectedNote) {
+            const generatedID = Math.random() % 100 + 1
+            setNotes([...notes, {id: generatedID, title: selectedNote?.title, content: selectedNote?.content, ownedBy: selectedNote?.ownedBy}])
+            setSelectedNoteID(generatedID)
+        }
+    }
+
     console.log(user)
     
     return (
         <div className="flex flex-1 overflow-hidden items-center justify-between flex-row max-lg:overflow-auto">
             <div className="max-md:w-full w-2/5 h-screen overflow-y-auto transition-all shadow-2xl border-r-2">
-                <div className="h-1/16 sticky top-0">
-                    <div className="flex justify-evenly items-center h-full">
-                        <Plus />
-                        <Minus />
-                        <Copy />
+                <div className="h-1/16 sticky top-0 bg-white border-b">
+                    <div className="flex justify-evenly items-center h-full *:cursor-pointer *:hover:scale-125 *:hover:text-gray-400 *:transition-all">
+                        <Plus onClick={createNewNote}/>
+                        <Minus onClick={deleteNote}/>
+                        <Copy onClick={copyNote}/>
                     </div>
                 </div>
-                <hr className="border "/>
                 {
                     fillteredList.map((note) => { 
                         if (note.id === selectedNoteID) return <NoteElement key={note.id} selected={true} title={note.title} content={note.content} onClick={() => setSelectedNoteID(note.id)}/>
@@ -111,17 +133,15 @@ export default function Notes() {
                 }
             </div>
             <div className="max-md:w-1/4 w-4/5 h-1/2">
-                { (selectedNoteID !== null && selectedNote !== null) ? 
+                { (selectedNoteID !== undefined && selectedNote !== undefined) ? 
                     <section className="flex flex-col items-center *:text-center">
-                        <h1>Selected {selectedNote?.id} ID</h1>
-                        <input className="w-1/2 text-4xl" type="text" value={selectedNote?.title} onChange={titleChangeHandle} id="title"/>
+                        <input className="w-1/2 text-4xl" type="text" value={selectedNote?.title} onChange={titleChangeHandle} placeholder="Title" id="title"/>
                         <textarea 
                             className="block w-7/8 m-4 flex-1" 
-                            id="content" rows={30} readOnly={false} onChange={contentChangeHandle} value={selectedNote?.content}>
-
+                            id="content" rows={30} readOnly={false} onChange={contentChangeHandle} value={selectedNote?.content} placeholder="Content">
                         </textarea>
                     </section>
-                : <h1>Choose a note</h1> }
+                : <h1 className="text-5xl">Choose a note</h1> }
             </div>
         </div>
     )
