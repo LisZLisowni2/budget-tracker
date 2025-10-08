92f8a74a24bc771822b4c4d735632f28f7f48aee
LisZLisowni2 <rafalkozikowski735@gmail.com>
2025-10-08 15:50:44 +0200

    Add NoteContext.tsx

---
+++ frontend/src/context/NoteContext.tsx
@@ -0,0 +1,62 @@
+import { useState, createContext, useContext, useEffect, ReactNode } from "react";
+import api from "../api";
+
+interface IChildren {
+    children?: ReactNode
+}
+
+interface INote {
+    title: string,
+    content: string,
+}
+
+interface INoteContext {
+	notes: INote[] | null,
+	loading: boolean,
+    handleNotes: () => Promise<void>,
+	// handleAddNote: () => Promise<void>,
+	// handleChangeNote: () => Promise<void>,
+	// handleDeleteNote: () => Promise<void>,
+	// handleCopyNote: () => Promise<void>,
+}
+
+const NoteContext = createContext<INoteContext | null>(null);
+
+export function NoteProvide({ children }: IChildren) {
+    const [notes, setNotes] = useState<INote[] | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const handleNotes = async () => {
+        await api.get('/notes/all')
+        .then(res => {
+            setNotes(res.data)
+        })
+    }
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                await handleNotes()
+            } catch (err) {
+                console.error(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchNotes()
+    }, [])
+
+    return (
+        <NoteContext.Provider value={{ notes, handleNotes, loading }}>
+            { children }
+        </NoteContext.Provider>
+    )
+}
+
+export function useNotes() {
+    const context = useContext(NoteContext)
+    if (!context) {
+    	throw new Error("useNotes must be used within an NoteProvide")
+    }
+    return context
+}
