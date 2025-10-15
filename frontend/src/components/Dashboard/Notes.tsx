1b40410217cbfc4a108612ad4fd166b8a4757b69
lislisowni2 <rafalkozikowski735@gmail.com>
2025-10-15 18:47:29 +0200

    Update Notes, handle Markdown

--- frontend/src/components/Dashboard/Notes.tsx
+++ frontend/src/components/Dashboard/Notes.tsx
@@ -2,6 +2,9 @@ import { useUser } from "../../context/UserContext"
 import { useState, MouseEventHandler, ChangeEvent } from "react"
 import { Plus, Minus, Copy } from "lucide-react"
 import { useNotes } from "../../context/NoteContext"
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+import Button from "../Button/Button"
 
 interface INoteElement {
     id?: number,
@@ -10,6 +13,10 @@ interface INoteElement {
     onClick: MouseEventHandler<HTMLButtonElement>,
     selected: boolean,
     ownedBy?: string
+}
+
+interface INoteViewer {
+    content?: string
 }
 
 function NoteElement({ title, content, onClick, selected }: INoteElement) {
@@ -23,9 +30,19 @@ function NoteElement({ title, content, onClick, selected }: INoteElement) {
     )
 }
 
+function NoteViewer({ content }: INoteViewer) {
+    return (
+        <div className="prose max-w-none border p-4">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                { content?.replace(/\\n/g, '\n') }
+            </ReactMarkdown>
+        </div>
+    )
+}
+
 export default function Notes() {
-    // TODO: Handle CRUD for notes
     sessionStorage.setItem("selectedDashboard", "3")
+    const [ isEditor, setIsEditor ] = useState<boolean>(false)
     const { user, loading: userLoading } = useUser()
     const { notes, loading: notesLoading, handleAddNote, handleChangeNote, handleCopyNote, handleDeleteNote } = useNotes()
     const [ selectedNoteID, setSelectedNoteID ] = useState<string | null>()
@@ -71,7 +88,7 @@ export default function Notes() {
 
     return (
         <div className="flex flex-1 max-md:flex-col overflow-hidden items-center justify-between flex-row max-lg:overflow-auto">
-            <div className="max-md:w-full w-2/5 max-md:h-64 h-screen overflow-y-auto transition-all shadow-sm md:shadow-2xl border-r-2">
+            <div className="max-md:w-full w-1/3 max-md:h-64 h-screen overflow-y-auto transition-all shadow-sm md:shadow-2xl border-r-2">
                 <div className="max-md:p-4 md:h-1/16 sticky top-0 bg-white border-b">
                     <div className="flex justify-evenly items-center h-full *:cursor-pointer *:hover:scale-125 *:hover:text-gray-400 *:transition-all">
                         <Plus onClick={createNewNote}/>
@@ -86,14 +103,19 @@ export default function Notes() {
                     })
                 }
             </div>
-            <div className="md:w-4/5 md:h-1/2">
+            <div className="md:h-1/2">
                 { (selectedNoteID !== undefined && selectedNote !== undefined) ? 
                     <section className="flex flex-col items-center *:text-center">
-                        <input className="md:w-1/2 text-4xl" type="text" value={selectedNote?.title} onChange={titleChangeHandle} placeholder="Title" id="title"/>
-                        <textarea 
-                            className="block md:w-7/8 md:m-4 flex-1" 
-                            id="content" rows={30} readOnly={false} onChange={contentChangeHandle} value={selectedNote?.content} placeholder="Content">
-                        </textarea>
+                        <div className="flex flex-row justify-evenly items-center">
+                            <input className="md:w-1/2 text-4xl p-4" type="text" value={selectedNote?.title} onChange={titleChangeHandle} placeholder="Title" id="title"/>
+                            <Button text={(isEditor) ? "Preview" : "Edit"} onClick={() => setIsEditor(!isEditor)}/>
+                        </div>
+                        <div className="w-full *:md:m-4 *:flex-1 flex flex-col justify-center">
+                            { (isEditor) ? <textarea 
+                                className="block" 
+                                id="content" rows={30} readOnly={false} onChange={contentChangeHandle} value={selectedNote?.content} placeholder="Content">
+                            </textarea> : <NoteViewer content={selectedNote?.content} /> }
+                        </div>
                     </section>
                 : <h1 className="text-3xl">Choose a note</h1> }
             </div>
