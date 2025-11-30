7cd9181d94f666d042dfd2cab0611f07bb54c900
lislisowni2 <rafalkozikowski735@gmail.com>
2025-11-30 14:55:34 +0100

    Update Overall.tsx

--- frontend/src/components/Dashboard/Overall.tsx
+++ frontend/src/components/Dashboard/Overall.tsx
@@ -1,10 +1,11 @@
-import { useMemo } from 'react';
+import { ChangeEvent, ReactNode, useMemo, useState } from 'react';
 import { Pie } from 'react-chartjs-2';
 import useTransactionsQuery from '@/hooks/useTransactionsQuery';
 import useUserQuery from '@/hooks/useUserQuery';
 import useNotesQuery from '@/hooks/useNotesQuery';
 import useGoalsQuery from '@/hooks/useGoalsQuery';
 import ErrorData from './ErrorData';
+import { DataRanges } from '@/types/dataRanges';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -40,7 +41,12 @@ interface IStatsItem {
 
 interface INotes {
     title: string;
-    notes: string[];
+    data: any[];
+}
+
+interface IGridItem {
+    children: ReactNode;
+    className?: string
 }
 
 function StatsItem({ title, value, currency, type }: IStatsItem) {
@@ -52,11 +58,9 @@ function StatsItem({ title, value, currency, type }: IStatsItem) {
               : 'bg-whtie text-black';
     return (
         <div
-            className={`w-full rounded-xl shadow-md hover:scale-105 transition-all ${bgColor}`}
+            className={`rounded-xl shadow-md hover:scale-105 transition-all flex flex-col justify-center items-center ${bgColor}`}
         >
-            <div className="flex flex-row justify-between">
-                <h3>{title}</h3>
-            </div>
+            <h3>{title}</h3>
             <h1 className="text-3xl">
                 {value}
                 {currency}
@@ -65,15 +69,15 @@ function StatsItem({ title, value, currency, type }: IStatsItem) {
     );
 }
 
-function Notes({ title, notes }: INotes) {
+function List({ title, data }: INotes) {
     return (
         <div className="overflow-hidden mx-2 h-80">
             <h2 className="text-xl lg:text-2xl">{title}</h2>
             <hr />
             <ul className="*:p-2 text-md">
-                {notes.map((note) => (
+                {data.map((item) => (
                     <li className="shadow-md p-1 m-2 hover:scale-105 transition-all">
-                        {note}
+                        {item}
                     </li>
                 ))}
             </ul>
@@ -81,17 +85,53 @@ function Notes({ title, notes }: INotes) {
     );
 }
 
+function GridItem({ children, className }: IGridItem) {
+    return (
+        <div className={`${className} text-lg lg:text-xl p-8 m-2 w-full h-full shadow-lg rounded-3xl overflow-hidden`}>
+            {children}
+        </div>
+    );
+}
+
 export default function Overall() {
     sessionStorage.setItem('selectedDashboard', '0');
-    // const [selectedRange, setSelectedRange] = useState<DataRanges>(
-    //     DataRanges.ONE_MONTH
-    // );
+    const [selectedRange, setSelectedRange] = useState<DataRanges>(
+        () => {
+            const savedRange = localStorage.getItem('selectedRange');
+            return savedRange ? JSON.parse(savedRange) : DataRanges.ONE_MONTH;
+        }
+    );
 
     const { data: user, isLoading: isUserLoading } = useUserQuery();
     const { data: notes, isLoading: isNotesLoading } = useNotesQuery();
     const { data: goals, isLoading: isGoalsLoading } = useGoalsQuery();
     const { data: transactions, isLoading: isTransactionsLoading } =
         useTransactionsQuery();
+
+    const handleRangeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedRange(event.target.value as DataRanges);
+        localStorage.setItem('selectedRange', JSON.stringify(event.target.value));
+    }
+
+    const calculateTime = useMemo(() => {
+        const now = new Date();
+        switch (selectedRange) {
+            case DataRanges.ONE_WEEK:
+                return now.setTime(now.getTime() - 1000 * 60 * 60 * 24 * 7);
+            case DataRanges.ONE_MONTH:
+                return now.setTime(now.getDate() - 1000 * 60 * 60 * 24 * 30);
+            case DataRanges.THREE_MONTHS:
+                return now.setTime(now.getDate() - 1000 * 60 * 60 * 24 * 90);
+            case DataRanges.SIX_MONTHS:
+                return now.setTime(now.getDate() - 1000 * 60 * 60 * 24 * 180);
+            case DataRanges.ONE_YEAR:
+                return now.setTime(now.getDate() - 1000 * 60 * 60 * 24 * 365);
+            case DataRanges.ALL_TIME:
+                return now.setTime(0);
+            default:
+                return now.setTime(0);
+        }
+    }, [selectedRange]);
 
     const latestNotes = 5;
     const sortedNotes = useMemo(
@@ -105,15 +145,55 @@ export default function Overall() {
                 .slice(0, latestNotes),
         [notes]
     );
+    const latestGoals = 5;
+    const sortedGoals = useMemo(
+        () =>
+            (goals || [])
+                .sort((goalA, goalB) => {
+                    const dateA = new Date(goalA.dateUpdate);
+                    const dateB = new Date(goalB.dateUpdate);
+                    return dateA.getTime() - dateB.getTime();
+                })
+                .slice(0, latestGoals),
+        [goals]
+    );
     const income = useMemo(() => {
         let result = 0;
         (transactions || []).map((transaction) => {
-            if (transaction.receiver) result += transaction.price;
+            if (
+                transaction.receiver &&
+                transaction.dateCreation.getTime() > calculateTime
+            )
+                result += transaction.price;
+        });
+
+        return result;
+    }, [transactions, selectedRange]);
+    const expensive = useMemo(() => {
+        let result = 0;
+        (transactions || []).map((transaction) => {
+            if (
+                !transaction.receiver &&
+                transaction.dateCreation.getTime() > calculateTime
+            )
+                result += transaction.price;
+        });
+
+        return result;
+    }, [transactions, selectedRange]);
+    const incomeNoLimit = useMemo(() => {
+        let result = 0;
+        (transactions || []).map((transaction) => {
+            if (
+                transaction.receiver &&
+                transaction.dateCreation.getTime() > calculateTime
+            )
+                result += transaction.price;
         });
 
         return result;
     }, [transactions]);
-    const expensive = useMemo(() => {
+    const expensiveNoLimit = useMemo(() => {
         let result = 0;
         (transactions || []).map((transaction) => {
             if (!transaction.receiver) result += transaction.price;
@@ -128,7 +208,7 @@ export default function Overall() {
         });
 
         return Array.from(categories);
-    }, [transactions]);
+    }, [transactions, selectedRange]);
     const valuesByCategories = useMemo(() => {
         let dictionary = new Map<string, number>();
         (categories || []).map((category) => {
@@ -143,7 +223,7 @@ export default function Overall() {
         });
         return Array.from(dictionary.values());
     }, [transactions]);
-    
+
     if (
         isUserLoading ||
         isGoalsLoading ||
@@ -199,65 +279,59 @@ export default function Overall() {
         },
     };
 
-    console.log(data);
-    console.log(options);
-
     return (
-        <div className="flex items-center flex-col h-full *:p-4 max-lg:overflow-auto">
-            <div className="flex max-md:flex-col w-full max-md:justify-center h-1/2">
-                <div className="flex flex-col w-full h-1/4 md:*:p-8 ">
-                    <div className="flex max-md:flex-col flex-row *:m-1 *:p-4">
-                        <StatsItem
-                            title="Total income"
-                            value={income}
-                            currency="zł"
-                            type="income"
-                        />
-                        <StatsItem
-                            title="Total costs"
-                            value={expensive}
-                            currency="zł"
-                            type="expense"
-                        />
-                    </div>
-                    <div className="max-mdflex-col flex flex-row *:m-1 *:p-4">
-                        <StatsItem
-                            title="Total profit"
-                            value={income - expensive}
-                            currency="zł"
-                            type="other"
-                        />
-                        <StatsItem
-                            title="Total balance"
-                            value={income - expensive}
-                            currency="zł"
-                            type="other"
-                        />
-                    </div>
-                </div>
-                <div className="flex-col lg:w-full h-1/4 max-md:hidden mt-8">
-                    <h1>Goals here</h1> {/* TODO: Add Goals for Overall */}
-                </div>
+        <div className="grid grid-rows-4 grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-4 *:justify-self-center *:self-center p-10 max-h-screen">
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 w-full h-full">
+                <StatsItem
+                    title="Total income"
+                    value={income}
+                    currency="zł"
+                    type="income"
+                />
+                <StatsItem
+                    title="Total costs"
+                    value={expensive}
+                    currency="zł"
+                    type="expense"
+                />
+                <StatsItem
+                    title="Total profit"
+                    value={income - expensive}
+                    currency="zł"
+                    type="other"
+                />
+                <StatsItem
+                    title="Total balance"
+                    value={incomeNoLimit - expensiveNoLimit}
+                    currency="zł"
+                    type="other"
+                />
             </div>
-            <div className="flex max-md:flex-col h-1/2 justify-evenly items-center w-full *:bg-white">
-                <div className="text-lg lg:text-xl w-full h-full p-8 m-2 shadow-2xl rounded-3xl flex justify-between overflow-hidden">
-                    <div className="w-full h-full flex justify-center">
-                        {categories.length > 0 &&
-                        valuesByCategories.length > 0 ? (
-                            <Pie data={data} options={options} />
-                        ) : (
-                            <p>Undefined data about expenses in that range.</p>
-                        )}
-                    </div>
-                </div>
-                <div className="text-lg lg:text-xl w-full h-full p-8 m-2 shadow-2xl rounded-3xl flex flex-col justify-between overflow-hidden">
-                    <h3 className="text-3xl">Notes</h3>
-                    <Notes
-                        title="Latest (last 5)"
-                        notes={sortedNotes.map((note) => note.title)}
-                    />
-                </div>
-            </div>
+            <GridItem>
+                <h3 className="text-4xl">Goals</h3>
+                <List
+                    title="Latest (last 5)"
+                    data={sortedGoals.map((goal) => goal.goalname)}
+                />
+            </GridItem>
+            <GridItem className='flex flex-col justify-center items-center'>
+                <select className='text-center' onChange={(e) => handleRangeChange(e)}>
+                    <option value="1W">Week</option>
+                    <option value="1M">Month</option>
+                    <option value="3M">3 Months</option>
+                    <option value="6M">6 Months</option>
+                    <option value="1Y">Year</option>
+                    <option value="ALL">All time</option>
+                </select>
+                <Pie data={data} options={options} />
+            </GridItem>
+            <GridItem>
+                <h3 className="text-3xl">Notes</h3>
+                <List
+                    title="Latest (last 5)"
+                    data={sortedNotes.map((note) => note.title)}
+                />
+            </GridItem>
         </div>
     );
 }
