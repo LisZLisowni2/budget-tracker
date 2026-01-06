81256d1dc267a1e5a9f21b432a95a460a26f1704
lislisowni2 <rafalkozikowski735@gmail.com>
2026-01-06 19:37:34 +0100

    Little change

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -7,5 +7,6 @@ declare namespace Cypress {
 }
 
 Cypress.Commands.add("clearDB", () => {
-    cy.request('POST', 'https://localhost:3000/test/cleanup')
+    const apiUrl = Cypress.env('apiUrl')
+    cy.request('POST', `${apiUrl}/test/cleanup`)
 })
